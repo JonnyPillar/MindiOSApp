d192ab321dd3c939d11e3969dce4e2da4b6bcc8e
Jonny Pillar <jonny.pillar@jonnypillar.co.uk>
2014-11-10 19:23:43 +0000

    Init of iOS app

---
+++ MindApp/MindApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MindApp
+//
+//  Created by Jonny Pillar on 10/11/2014.
+//  Copyright (c) 2014 Jonny Pillar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

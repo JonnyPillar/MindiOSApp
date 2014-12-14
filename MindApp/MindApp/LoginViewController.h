c7df7305bc1fad7e75100e4320efcd3b40febb8f
Jonny Pillar <jonny.pillar@jonnypillar.co.uk>
2014-12-14 13:15:56 +0000

    Added Login View and View Controller

---
+++ MindApp/MindApp/LoginViewController.h
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.h
+//  MindApp
+//
+//  Created by Jonny Pillar on 14/12/2014.
+//  Copyright (c) 2014 Jonny Pillar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UITextField *loginEmailAddressTextField;
+@property (strong, nonatomic) IBOutlet UITextField *loginPasswordTextField;
+@property (strong, nonatomic) IBOutlet UITextField *registerEmailAddressTextField;
+@property (strong, nonatomic) IBOutlet UITextField *registerPasswordTextField;
+@property (strong, nonatomic) IBOutlet UITextField *registerConfirmPasswordTextField;
+
+@end
---
+++ MindApp/MindApp/LoginViewController.m
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.m
+//  MindApp
+//
+//  Created by Jonny Pillar on 14/12/2014.
+//  Copyright (c) 2014 Jonny Pillar. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+@end

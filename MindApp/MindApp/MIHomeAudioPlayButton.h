4de9797bf6c4be011301335c1586f7a2e435d10e
Jonny Pillar <jonny.pillar@jonnypillar.co.uk>
2015-12-21 16:23:41 +0000

    Added remaining files from the old project into the workspace

---
+++ MindApp/MindApp/MIHomeAudioPlayButton.h
@@ -0,0 +1,20 @@
+//
+//  MIHomeAudioPlayButton.h
+//  MindApp
+//
+//  Created by Jonny Pillar on 24/02/2015.
+//  Copyright (c) 2015 Jonny Pillar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MIColour.h"
+#import "MIAudioPlayerProgress.h"
+#import "MIAudioPlayerItemInformation.h"
+
+@interface MIHomeAudioPlayButton : UIButton
+
+- (void) updateColourScheme:(MIColour*) colourScheme;
+- (void) updateProgress:(MIAudioPlayerProgress*) progressInformation;
+- (void) updateUIForNewItem:(MIAudioPlayerItemInformation *) itemInformation;
+
+@end
---
+++ MindApp/MindApp/MIHomeTableViewCell.h
@@ -0,0 +1,22 @@
+//
+//  MIHomeTableViewCell.h
+//  MindApp
+//
+//  Created by Jonny Pillar on 22/02/2015.
+//  Copyright (c) 2015 Jonny Pillar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AudioFile.h"
+
+@interface MIHomeTableViewCell : UITableViewCell
+
+@property (strong, nonatomic) IBOutlet UILabel *audioFileDuration;
+@property (strong, nonatomic) IBOutlet UILabel *audioFileTitle;
+@property (strong, nonatomic) IBOutlet UIView *cellIcon;
+
+@property (strong,nonatomic) AudioFile *cellAudioFile;
+
+-(void) updateCellIcon;
+
+@end

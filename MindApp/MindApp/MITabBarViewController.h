eb822d2baf884da5c0a5f9f423d37b683538c41f
Jonny Pillar <jonny.pillar@jonnypillar.co.uk>
2015-12-30 20:34:08 +0000

    Moved the view controller over to a tab bar controller

---
+++ MindApp/MindApp/MITabBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  MITabBarViewController.h
+//  MindApp
+//
+//  Created by Jonny Pillar on 30/12/2015.
+//  Copyright © 2015 Mind In Salford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MITabBarViewController : UITabBarController
+
+@end
